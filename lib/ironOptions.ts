8bc6c5b70c6b2eb5fe97da9d453c40a0f908fef1
bookland <bookland@porter.finance>
2022-07-04 14:58:28 -0500

    use env variable for IRON_PASSWORD

--- lib/ironOptions.ts
+++ lib/ironOptions.ts
@@ -1,6 +1,6 @@
 const ironOptions = {
     cookieName: 'siwe',
-    password: 'WWBrc5yQDTRunPxers77m8Z2akqMbMJgyw8KJiEaceJjD6VKUooiELnQkZmhgaJHj2QtAX7JhwqGFv2h5nYpjqRsEnVFswiiZzq4',
+    password: process.env.IRON_PASSWORD,
     cookieOptions: {
         secure: process.env.NODE_ENV === 'production',
     },
