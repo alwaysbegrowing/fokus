1bd5cb740eba18e597015253452b1e0033c928a4
bookland <bookland@porter.finance>
2022-07-06 18:26:43 -0500

    delete old server files

--- api/token.ts
+++ api/token.ts
@@ -8,16 +8,16 @@ const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioApiKey = process.env.TWILIO_API_KEY;
 const twilioApiSecret = process.env.TWILIO_API_SECRET;
 
-const identity = 'user';
 
 
 export default function handler(
     request: NextApiRequest,
     response: NextApiResponse,
 ) {
+    const { room_name: room, user_identity: identity } = request.body
 
     const videoGrant = new VideoGrant({
-        room: 'cool room',
+        room
     });
 
     // Create an access token which we will sign and return to the client,
@@ -26,12 +26,9 @@ export default function handler(
         twilioAccountSid,
         twilioApiKey,
         twilioApiSecret,
-        { identity: identity }
+        { identity }
     );
     token.addGrant(videoGrant);
-
-    // Serialize the token to a JWT string
-    console.log(token.toJwt());
 
     response.status(200).json({
         token: token.toJwt()
